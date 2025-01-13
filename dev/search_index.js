6ade156805b03e4c856fc16f4dadc423fdbf197a
Documenter.jl <documenter@juliadocs.github.io>
2025-01-13 22:17:34 +0000

    build based on a3cdda0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#HallPIC.jl","page":"HallPIC.jl","title":"HallPIC.jl","text":"","category":"section"},{"location":"","page":"HallPIC.jl","title":"HallPIC.jl","text":"Documentation for HallPIC.jl","category":"page"}]
+}

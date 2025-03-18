60f1080351b0f15d8de05e0b9d6950bbb1e03938
Thomas Marks <marksta@umich.edu>
2025-03-18 15:46:29 -0400

    gathering implemented with linear potential drop tests

--- src/HallPIC.jl
+++ src/HallPIC.jl
@@ -1,5 +1,24 @@
 module HallPIC
 
+using DataInterpolations: DataInterpolations, LinearInterpolation
+
+const q_e = 1.608e-19
+const N_A = 6.022e26
+const m_0 = 1 / N_A
+const q0_m0 = q_e / m_0
+const phi_0 = 300.0
+const x_0 = 0.05
+const E_0 = phi_0 / x_0
+const u_0 = sqrt(q_e * phi_0 / m_0)
+const t_0 = x_0 / u_0
+
+struct Particle{T<:AbstractFloat}
+	weight::T
+	pos::T
+	vel::T
+	acc::T
+end
+
 struct ParticleContainer{T<:AbstractFloat, I<:Integer}
     """
     T: Float32 or Float64
@@ -25,25 +44,31 @@ function ParticleContainer(::Type{T}, N, mass, charge) where T
 	)
 end
 
-
-function push_pos(pc::ParticleContainer, dt::AbstractFloat)
+function push_pos!(pc::ParticleContainer, dt::AbstractFloat)
     @inbounds for i in eachindex(pc.pos)
         pc.pos[i] = muladd(pc.vel[i], dt, pc.pos[i])
     end
 end
 
-function push_vel(pc::ParticleContainer, dt::AbstractFloat)
+function push_vel!(pc::ParticleContainer, dt::AbstractFloat)
     @inbounds for i in eachindex(pc.vel)
         pc.vel[i] = muladd(pc.acc[i], dt, pc.vel[i])
     end
 end
 
-function push(pc::ParticleContainer, dt::AbstractFloat)
+function push!(pc::ParticleContainer, dt::AbstractFloat)
     """
     Push particle container to next timestep using Leapfrog scheme
     """
-    push_vel(pc, dt)
-    push_pos(pc, dt)
+    push_vel!(pc, dt)
+    push_pos!(pc, dt)
+end
+
+function gather!(pc::ParticleContainer, E_itp::LinearInterpolation)
+	charge_to_mass = pc.charge / pc.mass
+	@inbounds for i in eachindex(pc.pos)
+		pc.acc[i] = charge_to_mass * E_itp(pc.pos[i])
+	end
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,37 +4,14 @@ using Test
 
 Documenter.doctest(hp)
 
-@testset "Harmonic oscillator" begin
-
-	function test_gather(pc::hp.ParticleContainer)
+function test_leapfrog(::Type{T}) where T
+	function leapfrog_gather(pc::hp.ParticleContainer)
 		@inbounds for i in eachindex(pc.pos)
 			pc.acc[i] = -pc.pos[i]
 		end
 	end
 
-	pc = hp.ParticleContainer(Float64, 1, 1, 1)
-
-	# Test 1: Initially-stationary particle remains at rest
-	x0 = 0.0
-	v0 = 0.0
 	dt = 0.2
-
-	pc.pos[1] = 0.0
-	pc.vel[1] = 0.0
-
-	# No acceleration at x = 0
-	test_gather(pc)
-	@test pc.acc[1] == 0
-
-	hp.push_vel(pc, -dt/2)
-
-	# No movement
-	hp.push(pc, dt)
-	@test pc.pos[1] == 0
-	@test pc.vel[1] == 0
-
-	# Test 2: simple harmonic oscillator starting at (x,v) = (1,0)
-	# Check that leapfrog is centered as expected
 	x0 = 1.0
 	v0 = 0
 	tmax = 5*pi
@@ -44,18 +21,37 @@ Documenter.doctest(hp)
 	pos = zeros(num_steps)
 	vel = zeros(num_steps)
 
+	pc = hp.ParticleContainer(T, 1, 1, 1)
+
+	# Test 1: Initially-stationary particle remains at rest
+	pc.pos[1] = 0.0
+	pc.vel[1] = 0.0
+
+	# No acceleration at x = 0
+	leapfrog_gather!(pc)
+	@test pc.acc[1] == 0
+
+	hp.push_vel!(pc, -dt/2)
+
+	# No movement
+	hp.push!(pc, dt)
+	@test pc.pos[1] == 0
+	@test pc.vel[1] == 0
+
+	# Test 2: simple harmonic oscillator starting at (x,v) = (1,0)
+	# Check that leapfrog is centered as expected
 	pc.pos[1] = x0
 	pc.vel[1] = v0
-	test_gather(pc)
+	leapfrog_gather!(pc)
 
 	# half step backward in velocity
-	hp.push_vel(pc, -dt/2)
+	hp.push_vel!(pc, -dt/2)
 	pos[1] = pc.pos[1]
 	vel[1] = pc.vel[1]
 
 	for i in 2:num_steps
-		test_gather(pc)
-		hp.push(pc, dt)
+		leapfrog_gather!(pc)
+		hp.push!(pc, dt)
 
 		pos[i] = pc.pos[1]
 		vel[i] = pc.vel[1]
@@ -69,4 +65,58 @@ Documenter.doctest(hp)
 	min, max = extrema(vel)
 	@test min >= -1
 	@test max <= 1
+end
+
+@testset "Harmonic oscillator" begin
+	for T in [Float32, Float64]
+		test_leapfrog(T)
+	end
+end
+
+function test_linear_drop(::Type{T}, V) where T
+	pc = hp.ParticleContainer(T, 1, 1, 1)
+	M = 100
+	L = 0.05
+	x = LinRange(0, L, M)
+	E = V / L * ones(M)
+
+	x_norm = x ./ hp.x_0
+	E_norm = E ./ hp.E_0
+
+	extrap = hp.DataInterpolations.ExtrapolationType.Constant
+	E_itp = hp.LinearInterpolation(E_norm, x_norm, extrapolation = extrap)
+
+	q = pc.charge * hp.q_e
+	m = pc.mass * hp.m_0
+
+	u_expected = sqrt(2 * q * V / m)
+	dt = 0.5 * L / u_expected / M
+	dt_norm = dt / hp.t_0
+
+	# pushback step
+	hp.gather!(pc, E_itp)
+	hp.push_vel!(pc, -dt_norm/2)
+
+	t = 0.0
+	while pc.pos[1] < x_norm[end] 
+		t += dt_norm
+		hp.gather!(pc, E_itp)
+		hp.push!(pc, dt_norm)
+	end
+
+	hp.push_vel!(pc, dt_norm/2)
+
+	a = pc.charge * E_norm[1] / pc.mass
+	u_exact = T(a * t)
+	x_exact = T(0.5 * a * t^2)
+
+	@test x_exact ≈ pc.pos[1]
+	@test u_exact ≈ pc.vel[1]
+end
+
+
+@testset "Linear potential drop" begin
+	for T in [Float32, Float64]
+		test_linear_drop(T, 600.0)
+	end
 end

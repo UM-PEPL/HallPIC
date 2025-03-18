3e728e47c15e684799b6ae89632bc02e3383699b
Thomas Marks <marksta@umich.edu>
2025-03-18 14:01:33 -0400

    Add leapfrog integrator

--- src/HallPIC.jl
+++ src/HallPIC.jl
@@ -1,7 +1,51 @@
 module HallPIC
 
-function add1(x)
-	return x + 1
+struct ParticleContainer{T<:AbstractFloat, I<:Integer}
+    """
+    T: Float32 or Float64
+    I: UInt8 (if less than 256 rxns) or UInt16
+    """
+    weight::Vector{T}
+    pos::Vector{T}
+    vel::Vector{T}
+    acc::Vector{T}
+    reactions::Vector{I}
+    mass::T
+    charge::UInt8
+end
+
+function ParticleContainer(::Type{T}, N, mass, charge) where T
+	weight = zeros(T, N)
+	pos = zeros(T, N)
+	vel = zeros(T, N)
+	acc = zeros(T, N)
+	rxn = UInt8[]
+	return ParticleContainer{T, eltype(rxn)}(
+		weight, pos, vel, acc, rxn, T(mass), eltype(rxn)(charge)
+	)
+end
+
+
+function push_pos(pc::ParticleContainer, dt::AbstractFloat)
+    @inbounds for i in eachindex(pc.pos)
+        #pc.pos[i] = muladd(pc.vel[i], dt, pc.pos[i])
+		pc.pos[i] += pc.vel[i] * dt
+    end
+end
+
+function push_vel(pc::ParticleContainer, dt::AbstractFloat)
+    @inbounds for i in eachindex(pc.vel)
+		pc.vel[i] += pc.acc[i] * dt
+        #pc.vel[i] = muladd(pc.acc[i], dt, pc.vel[i])
+    end
+end
+
+function push(pc::ParticleContainer, dt::AbstractFloat)
+    """
+    Push particle container to next timestep using Leapfrog scheme
+    """
+    push_vel(pc, dt)
+    push_pos(pc, dt)
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,73 @@
 using HallPIC: HallPIC as hp
 using Documenter
 using Test
+using CairoMakie: Makie as mk
 
-Documenter.doctest(hp)
+#Documenter.doctest(hp)
 
-@testset "HallPIC.jl" begin
-	@test hp.add1(1) == 2
+@testset "Harmonic oscillator" begin
+
+	function test_gather(pc::hp.ParticleContainer)
+		@inbounds for i in eachindex(pc.pos)
+			pc.acc[i] = -pc.pos[i]
+		end
+	end
+
+	pc = hp.ParticleContainer(Float64, 1, 1, 1)
+
+	# Test 1: Initially-stationary particle remains at rest
+	x0 = 0.0
+	v0 = 0.0
+	dt = 0.2
+
+	pc.pos[1] = 0.0
+	pc.vel[1] = 0.0
+
+	# No acceleration at x = 0
+	test_gather(pc)
+	@test pc.acc[1] == 0
+
+	hp.push_vel(pc, -dt/2)
+
+	# No movement
+	hp.push(pc, dt)
+	@test pc.pos[1] == 0
+	@test pc.vel[1] == 0
+
+	# Test 2: simple harmonic oscillator starting at (x,v) = (1,0)
+	# Check that leapfrog is centered as expected
+	x0 = 1.0
+	v0 = 0
+	tmax = 5*pi
+	num_steps = ceil(Int, tmax / dt)
+	t = range(0.0, tmax, length=num_steps)
+
+	pos = zeros(num_steps)
+	vel = zeros(num_steps)
+
+	pc.pos[1] = x0
+	pc.vel[1] = v0
+	test_gather(pc)
+
+	# half step backward in velocity
+	hp.push_vel(pc, -dt/2)
+	pos[1] = pc.pos[1]
+	vel[1] = pc.vel[1]
+
+	for i in 2:num_steps
+		test_gather(pc)
+		hp.push(pc, dt)
+
+		pos[i] = pc.pos[1]
+		vel[i] = pc.vel[1]
+	end
+
+	# Check that leapfrog is centered
+	min, max = extrema(pos)
+	@test min >= -1
+	@test max <= 1
+
+	min, max = extrema(vel)
+	@test min >= -1
+	@test max <= 1
 end
